872b7a36f02fc664d586c74ffd2c2e6460862937
Terry Creamer <trycrmr@gmail.com>
2018-10-18 14:19:46 -0400

    Refactors to default to csv-style output

--- breakserver.js
+++ breakserver.js
@@ -14,6 +14,7 @@ try {
     let timerID = Math.random()
     // console.log(timerID)
     console.time(`${timerID}`)
+    if(i === 1) console.log(`endpoint,startTime(epoch),iteration,requestDelay,error,statusCode,lap(ms)`)
     if(i < iterations) { // gross
       i++
 
@@ -24,7 +25,7 @@ try {
         })
       }, requestDelay)
     } else {
-      console.log(`We're done!`)
+      // console.log(`We're done!`)
       // process.exit()
     }
     return setTimeout(
@@ -32,10 +33,11 @@ try {
         let now = Date.now()
         request(endpoint, function (error, response, body) {
           // console.log(`-----${i}-----`)
-          console.log(`----------`)
-          console.log('error:', error); // Print the error if one occurred
-          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-          console.log(`Start: ${now} | Lap: ${Date.now() - now}ms`)
+          console.log(`${endpoint},${now},${i},${requestDelay},${error},${Date.now() - now}`)
+          // console.log(`----------`)
+          // console.log('error:', error); // Print the error if one occurred
+          // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+          // console.log(`Start: ${now} | Lap: ${Date.now() - now}ms`)
           // console.timeEnd(`${timerID}`)
           // console.log('body:', body); // Print the HTML for the Google homepage.
         })
