f1732f9be177d0c5573b5ccc7737898e84e4fee0
Terry Creamer <trycrmr@gmail.com>
2018-10-18 09:18:13 -0400

    Initial cut of breakserver

---
+++ breakserver.js
@@ -0,0 +1,53 @@
+// pingserver.js
+'use strict'
+
+// const http = require('http')
+const request = require('request');
+
+try {
+  const endpoint = process.argv[2]
+  const iterations = process.argv[3]
+  const requestDelay = process.argv[4]
+  let i = 1
+
+  const delayedRequest = ({requestDelay, endpoint}) => {
+    let timerID = Math.random()
+    // console.log(timerID)
+    console.time(`${timerID}`)
+    if(i < iterations) { // gross
+      i++
+
+      setTimeout(() => {
+        delayedRequest({
+          requestDelay,
+          endpoint
+        })
+      }, requestDelay)
+    } else {
+      console.log(`We're done!`)
+      // process.exit()
+    }
+    return setTimeout(
+      () => {
+        let now = Date.now()
+        request(endpoint, function (error, response, body) {
+          // console.log(`-----${i}-----`)
+          console.log(`----------`)
+          console.log('error:', error); // Print the error if one occurred
+          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+          console.log(`Start: ${now} | Lap: ${Date.now() - now}ms`)
+          // console.timeEnd(`${timerID}`)
+          // console.log('body:', body); // Print the HTML for the Google homepage.
+        })
+      }
+    , requestDelay)
+  }
+
+  delayedRequest({
+    requestDelay,
+    endpoint
+  })
+
+} catch(err) {
+  console.log(err)
+}
